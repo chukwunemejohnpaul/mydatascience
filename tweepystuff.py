e45d2bd489c0acabd2d86d95ce759f237b9b1a4f
chukwunemejohnpaul <47185877+chukwunemejohnpaul@users.noreply.github.com>
2019-03-09 16:09:59 -0800

    Update tweepystuff.py

--- tweepystuff.py
+++ tweepystuff.py
@@ -1,9 +1,9 @@
 import tweepy
 from textblob import TextBlob
-consumerkey = "ivUf9xcLrtuCSZ12dqdI1ASVK"
-consumersecret = "3z6oXGhsHBLCPkMHAl9wjOF0VaRO0oe3DkKiknPIAc3xNKNl9J"
-accesstoken = "987053000089882631-jfQLibVeo1ErvM1FeA2hLyQlCEp8ipT"
-accesstokensecret = "ZjWLk2WBo5y5Ctv9ZkGEtQtbV85Qex4AcPsmNBoN3hvvR"
+consumerkey = "your_secret"
+consumersecret = "your_secret"
+accesstoken = "your_secret"
+accesstokensecret = "your_secret"
 auth = tweepy.OAuthHandler(consumerkey,consumersecret)
 auth.set_access_token(accesstoken,accesstokensecret)
 api = tweepy.API(auth)
